393967e0d5239fb89c55e1140f5dcc5b4ed88544
hugosenari <hugosenari@gmail.com>
2022-03-12 04:35:22 -0300

    chore: re index notifishower

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = ''Small program to draw notifications on the screen in a highly customisable way'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."notifishower-master".url = "path:./master";
+  inputs."notifishower-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."notifishower-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."notifishower-v0_3_0".url = "path:./v0_3_0";
+  inputs."notifishower-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."notifishower-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."notifishower-v0_5_0".url = "path:./v0_5_0";
+  inputs."notifishower-v0_5_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."notifishower-v0_5_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."notifishower-v0_6_0".url = "path:./v0_6_0";
+  inputs."notifishower-v0_6_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."notifishower-v0_6_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."notifishower-v0_7_0".url = "path:./v0_7_0";
+  inputs."notifishower-v0_7_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."notifishower-v0_7_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_5_0/flake.nix
@@ -0,0 +1,66 @@
+{
+  description = ''Small program to draw notifications on the screen in a highly customisable way'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-notifishower-v0_5_0.flake = false;
+  inputs.src-notifishower-v0_5_0.owner = "PMunch";
+  inputs.src-notifishower-v0_5_0.ref   = "refs/tags/v0.5.0";
+  inputs.src-notifishower-v0_5_0.repo  = "notifishower";
+  inputs.src-notifishower-v0_5_0.type  = "github";
+  
+  inputs."imlib2".dir   = "nimpkgs/i/imlib2";
+  inputs."imlib2".owner = "riinr";
+  inputs."imlib2".ref   = "flake-pinning";
+  inputs."imlib2".repo  = "flake-nimble";
+  inputs."imlib2".type  = "github";
+  inputs."imlib2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."imlib2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."https://github.com/pmunch/x11".dir   = "nimpkgs/h/https://github.com/pmunch/x11";
+  inputs."https://github.com/pmunch/x11".owner = "riinr";
+  inputs."https://github.com/pmunch/x11".ref   = "flake-pinning";
+  inputs."https://github.com/pmunch/x11".repo  = "flake-nimble";
+  inputs."https://github.com/pmunch/x11".type  = "github";
+  inputs."https://github.com/pmunch/x11".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."https://github.com/pmunch/x11".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."npeg".dir   = "nimpkgs/n/npeg";
+  inputs."npeg".owner = "riinr";
+  inputs."npeg".ref   = "flake-pinning";
+  inputs."npeg".repo  = "flake-nimble";
+  inputs."npeg".type  = "github";
+  inputs."npeg".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."npeg".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."kiwi".dir   = "nimpkgs/k/kiwi";
+  inputs."kiwi".owner = "riinr";
+  inputs."kiwi".ref   = "flake-pinning";
+  inputs."kiwi".repo  = "flake-nimble";
+  inputs."kiwi".type  = "github";
+  inputs."kiwi".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."kiwi".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."termstyle".dir   = "nimpkgs/t/termstyle";
+  inputs."termstyle".owner = "riinr";
+  inputs."termstyle".ref   = "flake-pinning";
+  inputs."termstyle".repo  = "flake-nimble";
+  inputs."termstyle".type  = "github";
+  inputs."termstyle".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."termstyle".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-notifishower-v0_5_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-notifishower-v0_5_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
